a74fee95890c01fda04a7922ae3426df41906076
agj <ale@agj.cl>
2021-04-15 22:56:48 -0400

    Improved word get performance.

--- source/elm/Words.elm
+++ source/elm/Words.elm
@@ -6,12 +6,14 @@ module Words exposing
     , parse
     )
 
+import Dict exposing (Dict)
+import Dict.Extra as Dict
 import Random
-import Utils
+import Utils exposing (stringHead)
 
 
 type Words
-    = Words (List String)
+    = Words (Dict Char (List String))
 
 
 
@@ -20,10 +22,30 @@ type Words
 
 parse : String -> Words
 parse data =
+    let
+        wordToDict : String -> Dict Char (List String) -> Dict Char (List String)
+        wordToDict word dict =
+            let
+                initial =
+                    Utils.stringHead word
+                        |> Maybe.withDefault '?'
+            in
+            Dict.update initial (wordToList word) dict
+
+        wordToList : String -> Maybe (List String) -> Maybe (List String)
+        wordToList word listM =
+            case listM of
+                Just list ->
+                    Just (word :: list)
+
+                Nothing ->
+                    Just [ word ]
+    in
     data
         |> String.lines
         |> List.filter ((/=) "")
         |> List.map String.toLower
+        |> List.foldl wordToDict Dict.empty
         |> Words
 
 
@@ -33,29 +55,30 @@ parse data =
 
 exists : String -> Words -> Bool
 exists word (Words words) =
-    words |> List.member word
+    words |> Dict.any (\_ list -> List.member word list)
 
 
 candidate : String -> Words -> Bool
 candidate text (Words words) =
     let
+        initial =
+            stringHead text |> Maybe.withDefault '?'
+
         len =
             String.length text
+
+        matches w =
+            String.left len w == text
     in
-    words |> List.any (\w -> String.left len w == text)
+    words
+        |> Dict.get initial
+        |> Maybe.map (List.any matches)
+        |> Maybe.withDefault False
 
 
 get : Random.Seed -> Char -> Maybe Char -> Words -> ( String, Random.Seed )
 get seed initial incorporatesM (Words words) =
     let
-        initialPredicate word =
-            case String.uncons word of
-                Just ( l, rest ) ->
-                    l == initial
-
-                Nothing ->
-                    False
-
         incorporatesPredicate word =
             case incorporatesM of
                 Nothing ->
@@ -66,7 +89,8 @@ get seed initial incorporatesM (Words words) =
 
         filteredWords =
             words
-                |> List.filter initialPredicate
+                |> Dict.get initial
+                |> Maybe.withDefault []
                 |> List.filter incorporatesPredicate
     in
     Utils.randomItem seed filteredWords
