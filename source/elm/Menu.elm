18b3b9664d44adf9f694d8e938582e557671786b
agj <ale@agj.cl>
2021-04-24 14:17:38 -0400

    Developing Menu.

--- source/elm/Menu.elm
+++ source/elm/Menu.elm
@@ -1,13 +1,24 @@
-module Menu exposing (Menu, MenuAction(..), MenuLine, MenuText(..), MenuTextOptions, lines, start)
+module Menu exposing
+    ( Menu
+    , MenuAction(..)
+    , MenuLine
+    , MenuText(..)
+    , MenuTextOptions
+    , lines
+    , start
+    , toEnded
+    , toInGame
+    , toTitle
+    )
 
 import Game.GameMode exposing (GameMode(..))
 import Speed exposing (Speed)
 
 
-type
-    Menu
-    -- = Transitioning Float (List MenuLine) (List MenuLine)
-    = Stable (List MenuLine)
+type Menu
+    = Title GameMode Speed Transition
+    | InGame Transition
+    | Ended Transition
 
 
 type alias MenuLine =
@@ -27,31 +38,53 @@ type MenuAction
     = AuthorLink
     | ChangeGameMode GameMode
     | ChangeSpeed Speed
+    | Restart
+
+
+type Transition
+    = Stable
+    | Transitioning Float (List MenuLine)
 
 
 start : Menu
 start =
-    title HotseatMode Speed.Normal
+    Title SingleMode Speed.Normal Stable
+
+
+toTitle : GameMode -> Speed -> Menu -> Menu
+toTitle mode speed menu =
+    Title mode speed Stable
+
+
+toInGame : Menu -> Menu
+toInGame menu =
+    InGame Stable
+
+
+toEnded : Menu -> Menu
+toEnded menu =
+    Ended Stable
 
 
 lines : Menu -> List MenuLine
 lines menu =
     case menu of
-        Stable ls ->
-            ls
+        Title mode speed _ ->
+            titleLines mode speed
+
+        InGame _ ->
+            inGameLines
+
+        Ended _ ->
+            endedLines
 
 
 
--- setMode : GameMode -> Menu -> Menu
--- setMode mode menu =
---     case menu of
---         Stable lines ->
---             Stable title
 -- INTERNAL
 
 
-title : GameMode -> Speed -> Menu
-title mode speed =
+titleLines : GameMode -> Speed -> List MenuLine
+titleLines mode speed =
     let
         modeName =
             case mode of
@@ -85,18 +118,41 @@ title mode speed =
                 Speed.Slow ->
                     Speed.Normal
     in
-    Stable
-        [ [ PlainText "SPWORDS" boldDark
-          , PlainText " BY " boldDark
-          , PressableText "AGJ. " AuthorLink boldDark
-          ]
-        , [ PressableText ("[" ++ modeName ++ "]") (ChangeGameMode nextMode) normal
-          , PlainText " MODE. " normal
-          ]
-        , [ PressableText ("[" ++ speedName ++ "]") (ChangeSpeed nextSpeed) normal
-          , PlainText " SPEED. " normal
-          ]
-        ]
+    [ titleLine
+    , [ PressableText ("[" ++ modeName ++ "]") (ChangeGameMode nextMode) normal
+      , PlainText " MODE. " normal
+      ]
+    , [ PressableText ("[" ++ speedName ++ "]") (ChangeSpeed nextSpeed) normal
+      , PlainText " SPEED. " normal
+      ]
+    ]
+
+
+inGameLines : List MenuLine
+inGameLines =
+    [ restartLine
+    ]
+
+
+endedLines : List MenuLine
+endedLines =
+    [ titleLine
+    , restartLine
+    ]
+
+
+titleLine : MenuLine
+titleLine =
+    [ PlainText "SPWORDS" boldDark
+    , PlainText " BY " boldDark
+    , PressableText "AGJ. " AuthorLink boldDark
+    ]
+
+
+restartLine : MenuLine
+restartLine =
+    [ PressableText "[RESTART]" Restart normal
+    ]
 
 
 normal : MenuTextOptions
